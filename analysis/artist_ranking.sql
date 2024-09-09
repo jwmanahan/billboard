7097745d1c40ec5d4dfb88e43b2731cb1fb8f20a
jwmanahan <jwmanahan@gmail.com>
2024-09-09 01:44:11 -0400

    Backfill 2024-07-16 plus improvements

--- analysis/artist_ranking.sql
+++ analysis/artist_ranking.sql
@@ -7,104 +7,118 @@ WITH cte_billboard_lookback AS (
     FROM raw_db.billboard.billboard_ranking
 )
 
-SELECT
-    art.artist_id
-    , art.artist_name
+, cte_song_stats AS (
+    SELECT
+        ss.song_id
 
-    , COUNT_IF(
-        s2a.relationship_type = 'Lead artist'
-        AND bl.weeks_ago IS NOT NULL
-      ) AS recent_weeks_lead_artist
-    , COUNT_IF(
-        s2a.relationship_type IN ('Lead artist', 'Other main artist')
-        AND bl.weeks_ago IS NOT NULL
-      ) AS recent_weeks_main_artist
-    , COUNT_IF(bl.weeks_ago IS NOT NULL) AS recent_weeks_artist
-    , COUNT_IF(s2a.relationship_type = 'Lead artist')
-        + SUM(CASE WHEN s2a.relationship_type = 'Lead artist'
-                   THEN ss.untracked_weeks_on_chart
+        , COUNT_IF(bl.weeks_ago IS NOT NULL) AS recent_weeks_on_chart
+        , COUNT(1) + MAX(ss.untracked_weeks_on_chart) AS all_weeks_on_chart
+        , MIN(
+            CASE WHEN bl.weeks_ago IS NOT NULL THEN br.billboard_rank END
+          ) AS recent_peak_position
+        , MIN(br.billboard_rank) AS overall_peak_position
+        , MIN(br.billboard_observation_date) AS song_debut_date_in_data
+
+    FROM raw_db.billboard.song ss
+    LEFT JOIN raw_db.billboard.billboard_ranking br
+        ON ss.song_id = br.song_id
+    LEFT JOIN cte_billboard_lookback bl
+        ON br.billboard_observation_date = bl.billboard_observation_date
+        AND bl.weeks_ago <= 26 -- arbitrary definition of recency
+    GROUP BY 1
+)
+
+, cte_artist_stats AS (
+    SELECT
+        art.artist_id
+        , art.artist_name
+
+        , SUM(CASE WHEN s2a.relationship_type = 'Lead artist'
+                   THEN sst.recent_weeks_on_chart
               ELSE 0 END
-        ) AS all_weeks_lead_artist
-    , all_weeks_lead_artist
-        + COUNT_IF(s2a.relationship_type = 'Other main artist')
-        + SUM(CASE WHEN s2a.relationship_type = 'Other main artist'
-                   THEN ss.untracked_weeks_on_chart
+          ) AS recent_weeks_lead_artist
+        , SUM(CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
+                   THEN sst.recent_weeks_on_chart
               ELSE 0 END
-        ) AS all_weeks_main_artist
-    , COUNT(1) + SUM(ss.untracked_weeks_on_chart) AS all_weeks_artist
+          ) AS recent_weeks_main_artist
+        , SUM(sst.recent_weeks_on_chart) AS recent_weeks_artist
+        , SUM(CASE WHEN s2a.relationship_type = 'Lead artist'
+                   THEN sst.all_weeks_on_chart
+              ELSE 0 END
+          ) AS all_weeks_lead_artist
+        , SUM(CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
+                   THEN sst.all_weeks_on_chart
+              ELSE 0 END
+          ) AS all_weeks_main_artist
+        , SUM(sst.all_weeks_on_chart) AS all_weeks_artist
 
-    , COUNT(DISTINCT
-        CASE WHEN bl.weeks_ago IS NOT NULL
-                  AND s2a.relationship_type = 'Lead artist'
-             THEN br.song_id
-             ELSE NULL END
-      ) AS recent_num_songs_lead_artist
-    , COUNT(DISTINCT
-        CASE WHEN bl.weeks_ago IS NOT NULL
-                  AND s2a.relationship_type IN (
-                      'Lead artist'
-                      , 'Other main artist'
-                  )
-             THEN br.song_id
-             ELSE NULL END
-      ) AS recent_num_songs_main_artist
-    , COUNT(DISTINCT
-        CASE WHEN bl.weeks_ago IS NOT NULL
-        THEN br.song_id
-        ELSE NULL END
-      ) AS recent_num_songs_artist
-    , COUNT(DISTINCT
-        CASE WHEN s2a.relationship_type = 'Lead artist'
-             THEN br.song_id
-             ELSE NULL END
-      ) AS all_num_songs_lead_artist
-    , COUNT(DISTINCT
-        CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
-             THEN br.song_id
-             ELSE NULL END
-      ) AS all_num_songs_main_artist
-    , COUNT(DISTINCT br.song_id) AS all_num_songs_artist
+        , COUNT_IF(
+              s2a.relationship_type = 'Lead artist'
+              AND sst.recent_weeks_on_chart > 0
+          ) AS recent_num_songs_lead_artist
+        , COUNT_IF(
+              s2a.relationship_type IN ('Lead artist', 'Other main artist')
+              AND sst.recent_weeks_on_chart > 0
+          ) AS recent_num_songs_main_artist
+        , COUNT_IF(sst.recent_weeks_on_chart > 0) AS recent_num_songs_artist
 
-    , MIN(
-        CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
-                  AND bl.weeks_ago IS NOT NULL
-             THEN br.billboard_rank
-             ELSE NULL END
-      ) AS recent_peak_position_as_main_artist
-    , MIN(
-        CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
-             THEN br.billboard_rank
-             ELSE NULL END
-      ) AS all_peak_position_as_main_artist
+        , COUNT_IF(
+              s2a.relationship_type = 'Lead artist'
+              AND sst.all_weeks_on_chart > 0
+          ) AS all_num_songs_lead_artist
+        , COUNT_IF(
+              s2a.relationship_type IN ('Lead artist', 'Other main artist')
+              AND sst.all_weeks_on_chart > 0
+          ) AS all_num_songs_main_artist
+        , COUNT_IF(sst.all_weeks_on_chart > 0) AS all_num_songs_artist
 
-    , MIN(br.billboard_observation_date) AS debut_date_in_data -- even if not lead artist
+        , MIN(
+            CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
+                 THEN sst.recent_peak_position
+                 ELSE NULL END
+          ) AS recent_peak_position_as_main_artist
+        , MIN(
+            CASE WHEN s2a.relationship_type IN ('Lead artist', 'Other main artist')
+                 THEN sst.overall_peak_position
+                 ELSE NULL END
+          ) AS overall_peak_position_as_main_artist
 
+        , MIN(sst.song_debut_date_in_data) AS artist_debut_date_in_data -- even if not lead artist
+
+    FROM raw_db.billboard.artist art
+    INNER JOIN raw_db.billboard.song_to_artist s2a
+        ON art.artist_id = s2a.artist_id
+    INNER JOIN cte_song_stats sst
+        ON s2a.song_id = sst.song_id
+    GROUP BY 1,2
+)
+
+, cte_artist_ranking AS (
+    SELECT *
+        , ROW_NUMBER() OVER (
+            ORDER BY
+                recent_weeks_main_artist DESC
+                , recent_weeks_lead_artist DESC
+                , recent_weeks_artist DESC
+                , recent_num_songs_main_artist DESC
+                , recent_num_songs_lead_artist DESC
+                , recent_num_songs_artist DESC
+                , recent_peak_position_as_main_artist ASC
+                , all_weeks_main_artist DESC
+                , all_weeks_lead_artist DESC
+                , all_weeks_artist DESC
+                , all_num_songs_main_artist DESC
+                , all_num_songs_lead_artist DESC
+                , all_num_songs_artist DESC
+                , overall_peak_position_as_main_artist ASC
+                , artist_debut_date_in_data DESC
+                , artist_id ASC -- arbitrary final term for idempotence
+          ) AS artist_rank
+     FROM cte_artist_stats
+)
+
+SELECT *
     --, NULL AS cumulative_pct_female
-FROM raw_db.billboard.artist art
-INNER JOIN raw_db.billboard.song_to_artist s2a
-    ON art.artist_id = s2a.artist_id
-LEFT JOIN raw_db.billboard.song ss
-    ON s2a.song_id = ss.song_id
-LEFT JOIN raw_db.billboard.billboard_ranking br
-    ON s2a.song_id = br.song_id
-LEFT JOIN cte_billboard_lookback bl
-    ON br.billboard_observation_date = bl.billboard_observation_date
-    AND bl.weeks_ago <= 26 -- arbitrary definition of recency
-GROUP BY 1,2
-ORDER BY
-    recent_weeks_main_artist DESC
-    , recent_weeks_lead_artist DESC
-    , recent_weeks_artist DESC
-    , recent_num_songs_main_artist DESC
-    , recent_num_songs_lead_artist DESC
-    , recent_num_songs_artist DESC
-    , recent_peak_position_as_main_artist ASC
-    , all_weeks_main_artist DESC
-    , all_weeks_lead_artist DESC
-    , all_weeks_artist DESC
-    , all_num_songs_main_artist DESC
-    , all_num_songs_lead_artist DESC
-    , all_num_songs_artist DESC
-    , all_peak_position_as_main_artist ASC
-    , debut_date_in_data ASC
+FROM cte_artist_ranking
+ORDER BY artist_rank ASC
 ;
