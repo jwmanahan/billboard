293d58df42d245d947b63fa09b6b104d8d5df1f9
jwmanahan <jwmanahan@gmail.com>
2024-09-15 18:35:22 -0400

    Backfill a few weeks

--- upload/backfill_rank_by_name.py
+++ upload/backfill_rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2024-05-21
-    'A Bar Song (Tipsy)'
+rankings = [ # 2024-04-16
+    "Texas Hold 'Em"
     , 'I Remember Everything'
+    , 'Man Made A Bar'
+    , 'II Most Wanted'
     , 'Austin'
-    , "Texas Hold 'Em"
-    , 'Miles On It'
-    , 'Cowgirls'
     , 'Where The Wild Things Are'
+    , 'Burn It Down'
     , 'Where It Ends'
     , 'Wild Ones'
-    , 'Wildflowers And Wild Horses'
-    , 'Bulletproof'
-    , 'Back Then Right Now'
-    , 'Tucson Too Late'
-    , 'Halfway To Hell'
-    , "Wind Up Missin' You"
-    , 'We Ride'
-    , 'Dirt Cheap'
-    , 'Wine Into Whiskey'
-    , 'II Most Wanted'
-    , 'Spin You Around (1/24)'
-    , 'Take Her Home'
-    , 'Wondering Why'
-    , "Think I'm In Love With You"
-    , 'Your Place'
-    , "We Don't Fight Anymore"
-    , 'Indifferent'
-    , 'Let Your Boys Be Country'
-    , 'Holy Smokes'
-    , 'Love You Again'
-    , 'Smoke & A Light'
-    , 'Wranglers'
-    , "I'm Not Pretty"
-    , 'Where That Came From'
-    , 'Devil You Know'
-    , 'Young Love & Saturday Nights'
-    , 'Chevrolet'
-    , 'No Caller ID'
-    , 'Nine Ball'
-    , 'In Your Love'
-    , 'Boys Back Home'
-    , "Hangin' On"
-    , "What He'll Never Have"
-    , 'Cab In A Solo'
-    , "Can't Break Up Now"
     , 'Jolene'
-    , 'I Had Some Help'
-    , 'Worst Way'
-    , "I'm Gonna Miss Her"
-    , 'Life With You'
-    , "Should've Been A Cowboy" # Jelly Roll version
+    , 'Wildflowers And Wild Horses'
+    , "Levii's Jeans"
+    , 'Cowgirls'
+    , 'Outskirts'
+    , 'Tucson Too Late'
+    , 'Wine Into Whiskey'
+    , 'Halfway To Hell'
+    , 'Back Then Right Now'
+    , 'Spin You Around (1/24)'
+    , 'Tyrant'
+    , 'Blackbiird'
+    , "Wind Up Missin' You"
+    , 'Bulletproof'
+    , 'We Ride'
+    , '16 Carriages'
+    , 'Dirt Cheap'
+    , 'Coal'
+    , 'Cab In A Solo'
+    , 'Daughter'
+    , "We Don't Fight Anymore"
+    , 'Riiverdance'
+    , 'Take Her Home'
+    , 'Ameriican Requiem'
+    , "Think I'm In Love With You"
+    , 'Wondering Why'
+    , 'Protector'
+    , 'Smoke & A Light'
+    , 'Your Place'
+    , 'Holy Smokes'
+    , 'Love You, Miss You, Mean It'
+    , 'Alliigator Tears'
+    , 'Let Your Boys Be Country'
+    , 'Just For Fun'
+    , 'No Caller ID'
+    , 'Fearless (The Echo)'
+    , 'My Rose'
+    , 'Deeper Well'
+    , 'Love You Again'
+    , "I'm Not Pretty"
+    , 'Young Love & Saturday Nights'
 ]
