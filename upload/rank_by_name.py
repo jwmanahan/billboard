8c2882fdc8593e106c6b88764aada127a21c0b52
jwmanahan <jwmanahan@gmail.com>
2024-12-24 23:54:52 -0500

    New week and disambiguate by artist ID in coolest chart

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -2,51 +2,51 @@ rankings = [ # 2024-12-17
     'A Bar Song (Tipsy)'
     , 'I Had Some Help'
     , 'Love Somebody'
-    , 'I Am Not Okay'
     , 'High Road' # Wetzel & Murph
-    , 'Lies Lies Lies'
+    , 'I Am Not Okay'
     , "Wind Up Missin' You"
-    , 'You Look Like You Love Me'
-    , 'Miles On It'
-    , 'I Never Lie'
-    , 'Liar'
-    , "I'm Gonna Love You"
-    , 'Pour Me A Drink'
-    , 'Good News'
-    , '4x4xU'
-    , "Think I'm In Love With You"
+    , 'Lies Lies Lies'
     , 'Indigo'
+    , 'I Never Lie'
+    , 'Pour Me A Drink'
+    , 'You Look Like You Love Me'
+    , 'Liar'
+    , '4x4xU'
+    , "I'm Gonna Love You"
+    , 'Good News'
+    , "Think I'm In Love With You"
     , 'Guy For That'
     , 'Cowboy Songs'
-    , 'High Road' # Zach Bryan
-    , 'Am I Okay?'
     , "Boys Back Home"
+    , 'Am I Okay?'
     , "I Ain't Sayin'"
+    , 'High Road' # Zach Bryan
     , '28'
-    , 'Lonely Road'
-    , 'Georgia Ways'
     , 'Holy Smokes'
     , 'Ends Of The Earth'
-    , "Somethin' 'Bout A Woman"
-    , 'Bigger Houses'
-    , "Fix What You Didn't Break"
-    , 'Gonna Love You'
-    , "This Town's Been Too Good To Us"
-    , 'House Again'
-    , "Don't Mind If I Do"
-    , 'Relapse'
-    , 'Devil You Know'
     , "Weren't For The Wind"
+    , 'Bigger Houses'
+    , "Don't Mind If I Do"
+    , 'House Again'
+    , "This Town's Been Too Good To Us"
+    , "Somethin' 'Bout A Woman"
+    , "Fix What You Didn't Break"
     , 'Texas'
+    , 'Devil You Know'
+    , 'Georgia Ways'
     , 'Damn Good Day To Leave'
-    , 'Porch Light'
-    , 'Just Like Johnny'
-    , 'Neon Moon'
-    , 'Last Of My Kind'
-    , 'Thoughts Of You'
-    , 'Losers'
-    , 'Cowboys Cry Too'
-    , 'A Lot More Free'
-    , 'Bad Luck'
+    , 'Relapse'
     , 'Maybe This Christmas'
+    , 'Gonna Love You'
+    , 'Porch Light'
+    , 'Thoughts Of You'
+    , 'Just Like Johnny'
+    , 'A Lot More Free'
+    , 'Forever To Me'
+    , 'Bad Luck'
+    , 'Last Of My Kind'
+    , 'Driving Home For Christmas'
+    , 'Backseat Driver'
+    , 'Losers'
+    , 'Friends Like That'
 ]
