33c1843959db2af484c34a5b00435c0ef9cd7301
jwmanahan <jwmanahan@gmail.com>
2025-01-07 20:46:09 -0500

    Add new week

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2024-12-31
+rankings = [ # 2025-01-07
     'A Bar Song (Tipsy)'
     , 'I Had Some Help'
     , 'Love Somebody'
     , 'High Road' # Wetzel & Murph
+    , 'Smile'
     , 'I Am Not Okay'
     , "Wind Up Missin' You"
     , 'Lies Lies Lies'
-    , 'I Never Lie'
-    , 'Indigo'
     , 'Pour Me A Drink'
+    , 'I Never Lie'
     , 'Liar'
     , "I'm Gonna Love You"
     , '4x4xU'
     , 'Good News'
+    , 'Indigo'
     , "Think I'm In Love With You"
     , 'Guy For That'
+    , 'Am I Okay?'
     , "I Ain't Sayin'"
     , "Boys Back Home"
-    , 'Am I Okay?'
-    , '28'
-    , 'Cowboy Songs'
-    , 'Maybe This Christmas'
-    , 'High Road' # Zach Bryan
-    , 'Holy Smokes'
     , 'Ends Of The Earth'
+    , 'Holy Smokes'
+    , 'High Road' # Zach Bryan
+    , 'Cowboy Songs'
+    , '28'
     , "Weren't For The Wind"
-    , 'Driving Home For Christmas'
     , 'Bigger Houses'
-    , 'House Again'
-    , "This Town's Been Too Good To Us"
-    , "Don't Mind If I Do"
-    , 'Devil You Know'
     , "Fix What You Didn't Break"
+    , "This Town's Been Too Good To Us"
+    , 'House Again'
+    , "Don't Mind If I Do"
     , 'Texas'
+    , 'Devil You Know'
     , 'Relapse'
-    , 'Damn Good Day To Leave'
     , "Somethin' 'Bout A Woman"
-    , 'Porch Light'
-    , 'Georgia Ways'
-    , 'A Lot More Free'
+    , 'Damn Good Day To Leave'
     , 'Just Like Johnny'
-    , 'Last Of My Kind'
-    , 'Bad Luck'
-    , 'Losers'
-    , 'Thoughts Of You'
-    , 'Friends Like That'
     , 'Backseat Driver'
-    , 'Gonna Love You'
+    , 'A Lot More Free'
     , 'Forever To Me'
     , 'Cowboys Cry Too'
+    , 'Porch Light'
+    , 'Losers'
+    , 'Friends Like That'
+    , 'Single Again'
+    , 'Last Of My Kind'
+    , 'Gonna Love You'
+    , 'Neon Moon'
+    , "Darlin'"
+    , 'Hometown Home'
 ]
