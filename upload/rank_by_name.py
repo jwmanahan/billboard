3c301979affaff21bf7bf85c13888625a91237bc
jwmanahan <jwmanahan@gmail.com>
2025-03-04 20:09:19 -0500

    New week

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2025-02-25
+rankings = [ # 2025-03-04
     'A Bar Song (Tipsy)'
     , 'Love Somebody'
     , 'I Had Some Help'
     , "I'm The Problem"
-    , 'High Road' # Wetzel & Murph
     , 'Liar'
-    , 'Smile'
+    , 'High Road' # Wetzel & Murph
     , 'I Never Lie'
+    , 'Smile'
     , "I'm Gonna Love You"
     , 'Indigo'
-    , '4x4xU'
-    , 'Good News'
     , 'Worst Way'
+    , 'Good News'
     , "I Ain't Sayin'"
-    , 'Guy For That'
+    , '4x4xU'
     , 'Am I Okay?'
-    , 'Please Please Please' # ft. Parton
     , 'Backseat Driver'
-    , 'Bad State Of Mind'
     , "Weren't For The Wind"
-    , 'Dear Miss'
-    , 'Haunted'
-    , 'Holy Smokes'
     , "Somethin' 'Bout A Woman"
     , "Fix What You Didn't Break"
+    , 'Holy Smokes'
     , 'Texas'
-    , "Don't Mind If I Do"
+    , 'Haunted'
     , "This Town's Been Too Good To Us"
+    , "Don't Mind If I Do"
     , 'Ends Of The Earth'
-    , 'Forever To Me'
-    , 'Blue Jean Baby'
     , 'Hometown Home'
+    , 'Forever To Me'
+    , 'Dear Miss'
+    , 'Bad State Of Mind'
     , 'Friends Like That'
-    , 'House Again'
     , 'Relapse'
+    , 'Blue Jean Baby'
+    , 'House Again'
     , 'Just Like Johnny'
     , "Please Don't Go"
+    , 'Happen To Me'
+    , 'Whiskey Drink'
     , 'Cowboys Cry Too'
+    , '3,2,1'
     , 'Use Me'
     , 'High Road' # Zach Bryan
-    , 'Whiskey Drink'
-    , 'Oil Money'
-    , 'Single Again'
-    , 'Coming Home'
-    , 'Bad Luck'
-    , 'Baggage'
-    , 'She Hates Me'
-    , 'Tough People'
     , 'After All The Bars Are Closed'
+    , 'Nobody Likes Your Girlfriend'
+    , 'Bad Luck'
+    , 'Coming Home'
+    , 'Single Again'
+    , 'Tough People'
+    , 'Baggage'
     , 'Low Road'
+    , 'Better Me For You (Brown Eyes)'
 ]
