bc8ca36df409fd11d252b9c1596f6f1a232ba505
jwmanahan <jwmanahan@gmail.com>
2025-12-09 20:54:55 -0500

    New week

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2025-12-02
+rankings = [ # 2025-12-09
     "Choosin' Texas"
     , 'I Got Better'
     , "I'm The Problem"
     , 'What I Want'
     , 'Just In Case'
     , "Don't Mind If I Do"
-    , 'Somewhere Over Laredo'
-    , 'Hell At Night'
-    , '6 Months Later'
     , 'Happen To Me'
     , '20 Cigarettes'
+    , '6 Months Later'
+    , 'Hell At Night'
+    , 'Somewhere Over Laredo'
     , 'House Again'
-    , "Travelin' Soldier"
     , 'Better Me For You (Brown Eyes)'
-    , 'What Kinda Man'
-    , 'Last One To Know'
-    , 'The Fall'
-    , 'Back In The Saddle'
-    , 'Cold'
-    , 'Hard Fought Hallelujah'
-    , 'Amen'
-    , 'Favorite Country Song'
     , '3,2,1'
-    , 'Days Like These'
-    , 'How Far Does A Goodbye Go'
+    , 'The Fall'
+    , "Travelin' Soldier"
+    , 'Amen'
+    , 'Last One To Know'
+    , 'Favorite Country Song'
+    , 'Back In The Saddle'
     , 'Cowgirl'
+    , 'Days Like These'
+    , 'What Kinda Man'
+    , 'How Far Does A Goodbye Go'
+    , 'Hard Fought Hallelujah'
     , "I Ain't Coming Back"
+    , "Stay Country Or Die Tryin'"
     , 'Beautiful Things'
     , 'Good Times & Tan Lines'
-    , "Stay Country Or Die Tryin'"
-    , 'Ends Of The Earth'
+    , 'Bad State Of Mind'
+    , 'Misery'
     , "It Won't Be Long"
-    , 'Holy Water'
+    , 'Cold'
+    , 'Ends Of The Earth'
+    , 'Shit Hill'
+    , 'Port A'
+    , 'Withdrawals'
+    , 'West Texas Degenerate'
     , "Ain't A Bad Life"
-    , 'South Of Sanity'
-    , 'Painted You Pretty'
     , "Time's Ticking"
-    , 'A Song To Sing'
+    , 'Holy Water'
+    , 'Painted You Pretty'
     , 'Keeps Me Sane'
-    , 'Bottomland'
+    , 'South Of Sanity'
+    , 'Sunflower' # Treaty Oak Revival
+    , 'Happy Face'
+    , 'A Song To Sing'
+    , 'Stay A While'
     , 'Need To'
     , 'Sober'
-    , "Let 'Em Know"
-    , "Girl You're Taking Home"
-    , 'Damned If I Do'
-    , "People Hatin'"
-    , 'I Dare You'
-    , 'Change My Mind'
-    , 'Use Me'
-    , 'I Sit In Parks'
 ]
