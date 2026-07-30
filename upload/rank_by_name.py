dd5dc057059b7d6f9399637fdfc6aa65ebcb503d
jwmanahan <jwmanahan@gmail.com>
2026-07-29 22:51:24 -0400

    New week & added a chart for weight by position

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2026-07-21
+rankings = [ # 2026-07-28
     "Choosin' Texas"
     , 'I Knew It, I Knew You'
-    , 'Be Her'
     , 'Boston'
     , "I Can't Love You Anymore"
+    , 'Be Her'
     , "Don't We"
     , 'Be By You'
-    , 'Sleepless In A Hotel Room'
     , "Don't Tell On Me"
     , 'Loving Life Again'
+    , 'Sleepless In A Hotel Room'
     , 'Something To Lose'
     , 'Mexico Honey'
-    , 'Hate How You Look'
-    , "Phone, Keys, Wallet"
+    , 'Chevy Silverado'
     , 'Woman'
     , 'Rethink Some Things'
+    , 'Hate How You Look'
+    , "Phone, Keys, Wallet"
     , 'McArthur'
     , 'Dandelion'
-    , 'Chevy Silverado'
+    , 'Cowgirl' # Shaboozey
     , 'Think As You Drunk'
-    , 'Damned If I Do'
     , 'Carry On'
-    , 'Bottom Of Your Boots'
     , 'I Want You'
-    , 'Who Told You That'
+    , 'Damned If I Do'
+    , 'Bottom Of Your Boots'
+    , 'South Of Sanity'
     , 'Empty Words'
     , "No Need For Leavin'"
-    , 'South Of Sanity'
-    , 'Cowgirl' # Shaboozey
     , 'Kingdom Of Fear'
+    , 'Hands Up'
     , 'Sunburn'
     , 'Another Drink'
-    , 'Dry Spell'
     , 'My Way'
     , 'Painted You Pretty'
-    , 'Hands Up'
+    , 'Dry Spell'
     , 'Bet On That'
     , "Get To Drinkin'"
-    , 'Hurts Like You'
     , "Kid Myself"
     , 'Country And She Knows It'
-    , 'Say So'
-    , 'Ride, Ride, Ride'
-    , "Demons In Your Choir"
     , 'String By'
+    , 'Hurts Like You'
+    , 'Who Told You That'
     , 'All My Exes'
+    , 'Ride, Ride, Ride'
+    , 'Say So'
     , "Killin' Me"
-    , 'Go Again'
-    , 'What You Want'
     , 'Medicine'
+    , "Wish I Didn't Know Now" # Langley
+    , 'What You Want'
+    , "Take Me Back (Leave Me There)"
 ]
