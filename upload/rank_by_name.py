e317c9e93ef129bcafb1ea47080fb3e370fea4e4
jwmanahan <jwmanahan@gmail.com>
2026-08-18 23:51:41 -0400

    New week

--- upload/rank_by_name.py
+++ upload/rank_by_name.py
@@ -1,52 +1,52 @@
-rankings = [ # 2026-08-11
+rankings = [ # 2026-08-18
     "Choosin' Texas"
-    , "Been By Now"
     , 'I Knew It, I Knew You'
+    , "Been By Now"
     , 'Boston'
     , "I Can't Love You Anymore"
     , 'Be Her'
     , 'Be By You'
+    , 'Jaded'
     , "Don't We"
     , 'Loving Life Again'
-    , 'Sleepless In A Hotel Room'
-    , 'Cowgirl' # Shaboozey
-    , "Don't Tell On Me"
     , 'Mexico Honey'
-    , "Phone, Keys, Wallet"
+    , 'Cowgirl' # Shaboozey
     , 'Something To Lose'
-    , 'Rethink Some Things'
+    , "Phone, Keys, Wallet"
     , 'Chevy Silverado'
+    , 'Rethink Some Things'
+    , "Don't Tell On Me"
     , 'Woman'
     , 'Hate How You Look'
     , 'Carry On'
-    , 'McArthur'
     , 'Think As You Drunk'
-    , 'Bottom Of Your Boots'
+    , 'McArthur'
     , 'Empty Words'
     , 'Another Drink'
     , 'South Of Sanity'
-    , 'Sunburn'
-    , 'Hands Up'
-    , "No Need For Leavin'"
+    , 'Bottom Of Your Boots'
     , 'String By'
-    , "Kid Myself"
-    , 'Painted You Pretty'
-    , 'Country And She Knows It'
-    , 'My Way'
-    , 'Bet On That'
-    , "Get To Drinkin'"
-    , 'Kingdom Of Fear'
-    , 'All My Exes'
-    , 'Dry Spell'
-    , 'Hurts Like You'
-    , 'Who Told You That'
-    , "Bullets & Blades"
+    , "No Need For Leavin'"
     , "Take Me Back (Leave Me There)"
+    , 'Hands Up'
+    , 'Bet On That'
+    , "Kid Myself"
+    , 'Country And She Knows It'
+    , 'Painted You Pretty'
+    , 'All My Exes'
+    , 'Kingdom Of Fear'
+    , "Get To Drinkin'"
+    , 'My Way'
     , 'I Want You'
     , 'Ride, Ride, Ride'
+    , 'Who Told You That'
     , 'Say So'
+    , "What You Want"
     , 'Medicine'
+    , "Lean In, Kiss Me"
     , "Killin' Me"
+    , 'Hurts Like You'
+    , "Imagine That"
     , "Nothin' Better To Do"
-    , "Burn It Down" # Shaboozey
+    , "I Know I Know"
 ]
